b976079bd37e0f648d80ce69a038f5d4f5b20a4b
Elton Mehmeti <97539294+EltonMehmeti@users.noreply.github.com>
2022-08-18 11:18:04 +0200

    OurServices

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,20 @@ import React from 'react'
 import About from './components/about/About';
 import Header from './components/header/Header'
 import Nav from './components/nav/Nav'
+import OurPortfolio from './components/ourportfolio/OurPortfolio';
 import OurServices from './components/ourservices/OurServices';
+
 <script src="https://unpkg.com/flowbite@1.4.4/dist/flowbite.js"></script>
 const App = () => {
     return (
       <>
+      <div className='overflow-x-hidden'>
       <Nav />
       <Header />
         <About />
         <OurServices />
+        <OurPortfolio />
+   </div>
       </>
     );
   }
