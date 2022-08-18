4001f4b344bf270dcf020eeb10b0b3f2998ec677
Albert <ialbertbislimi@gmail.com>
2022-08-18 14:09:20 +0200

    hero changes

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -17,10 +17,8 @@ business goal come true</h1>
             <button type="button" class="text-white bg-blue-700 hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 font-medium rounded-full text-lg px-11 py-2.5 text-center mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Start Project
             </button>
             </a>
-          <a href="/#" class="flex items-center">
-            <img src={Frame} alt="Logo"/>
-          </a>
         </div>
+        <img src={Frame} class="flex items-center w-full" alt="Logo"/>
     </div> 
   )
 }
