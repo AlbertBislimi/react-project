77b9f7b702241200f8fb23347c33077a8c9fdec8
Elton Mehmeti <97539294+EltonMehmeti@users.noreply.github.com>
2022-08-19 18:54:58 +0200

    Refactored

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -5,34 +5,33 @@ import Aboutus from '../../assets/Aboutus.png'
 
 const About = () => {
   return (
-    <div class="h-screen flex items-center bg-white" id="a">
-      <div class="flex-1 flex items-center justify-center relative h-full" id="a-left">
-        <div className="a-card bg"></div>
-        <div class="w-10/12 h-auto rounded-3xl relative overflow-hidden">
-          <img
-            src={Aboutus}
-            alt=""
-            class="w-auto object-cover"
-          />
-        </div>
-      </div>
-      <div class="flex-1">
-        <h3 class="font-Nunito text-cyan-400">About Us</h3>
-        <h2 class="font-Nunito">Our Teammate</h2>
-        <p className="a-sub sm:text-center">
-        We move with make a Creative Strategy for help your business goal, we help to improve your income by a services we have. make your content look interesting and make people look for your business.
+   <div name="a" className="flex-col py-8 px-4 mx-auto  text-center lg:text-ellipsis lg:mr-7 lg:w-full lg:flex-row lg:h-screen lg:flex  lg:items-center" >
+    <div name="right" className=" lg:float-right lg:px-9"> 
+   <h2 className="mb-4 font-semibold text-xl md:text-2xl lg:text-3xl text-blue-700">About Us</h2>
+   <h1 className="mb-8 font-bold text-4xl  tracking-tight  md:text-3xl lg:text-5xl dark:text-white">Our Teammate</h1>
+   <p className="mb-8 text-lg text-left font-normal  text-gray-500  lg:text-justify sm:px-16 xl:px-48 lg:text-lg">
+  We move with make a Creative Strategy for help your business goal, we help to improve your income by a services we have. make your content look interesting and make people look for your business.
 
-        We move with make a Creative Strategy for help your business goal, we help to improve your income by a services we have. make your content look interesting and make people look for your business
-        </p>
-        <div id="hero_btn_container">
- 
-          <button type="button" class="text-white bg-blue-700 hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 font-medium rounded-full text-sm px-5 py-2.5 text-center mr-8 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 ">About Us</button>
-          <button type="button" class="text-blue-700 hover:text-white border border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2 dark:border-blue-500 dark:text-blue-500 dark:hover:text-white dark:hover:bg-blue-600 dark:focus:ring-blue-800">Our Services</button>
-                        
-        </div>
-      </div>
-    </div>
+  We move with make a Creative Strategy for help your business goal, we help to improve your income by a services we have. make your content look interesting and make people look for your business
+  </p>
+  <button type="button" className="text-white bg-blue-700 hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 font-medium rounded-full text-sm px-5 py-2.5 text-center mr-8 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 ">About Us</button>
+    <button type="button" className="text-blue-700 hover:text-white border border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2 dark:border-blue-500 dark:text-blue-500 dark:hover:text-white dark:hover:bg-blue-600 dark:focus:ring-blue-800">Our Teammate</button>
+     </div> 
+
+
     
+    <img
+      src={Aboutus}
+      alt=""
+      className="lg:shrink md:shrink lg:w-3/5 md:w-auto md:ml-10 lg:object-cover"
+    />
+  
+    
+
+
+    
+  </div>
+ 
   );
 };
 
