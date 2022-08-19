2f8c269a9242e94f670107aae49ea075eaee94ea
qendriimrexhepi <qendriimrexhepi@gmail.com>
2022-08-19 16:11:31 +0200

    SomeChanges

--- src/components/ourportfolio/OurPortfolio.jsx
+++ src/components/ourportfolio/OurPortfolio.jsx
@@ -7,26 +7,26 @@ import portfolio2 from '../../assets/portfolio2.png'
 
 const Portfolio = () => {
   return (
-  <div name='portfolio' className='w-full mt-20'>
+  <div name='portfolio' className='w-full mt-10'>
 
-      <div className='max-w-[1240px] mx-auto relative'>
-          <div className='px-4 py-10'>
+      <div className='max-w-[1240px] mx-auto relative lg:py-10 lg:px-12'>
+          <div className='py-8 px-4 mx-auto max-w-screen-xl text-center lg:py-16 lg:px-12'>
               <h2 className="text-xl pt-8 text-blue-600 text-center">Our Portfolio</h2>
-              <h3 className='text-5xl font-bold py-6 text-black text-center'>What do we do</h3>
-              <p className='py-4 text-lg font-light mx-56 text-black text-center'>all projects that we have already done, proven can help to use more comfortable, then can used by client from our business.</p>
+              <h3 className='text-3xl font-bold py-2 sm:text-5xl text-black text-center'>What do we do</h3>
+              <p className='text-lg font-normal pt-4 text-gray-500 lg:text-xl sm:px-16 xl:px-48 dark:text-gray-400'>all projects that we have already done, proven can help to use more comfortable, then can used by client from our business.</p>
           </div>
 
           <div className='grid grid-cols-1 lg:grid-cols-3 gap-x-8 gap-y-16 px-4 pt-10 sm:pt-10 text-black'>
-            <div className='relative'>
-              <img src={portfolio} alt="" />
+            <div className='relative p-5'>
+              <img src={portfolio} className="items-center w-full" alt="" />
               <h1 class="absolute text-xl font-medium text-white bottom-8 left-8">Design Byte App</h1>
             </div> 
             <div className='relative'>
-              <img src={portfolio1} alt="" />
-              <h1 class="absolute text-xl font-medium text-white bottom-10 left-8">Cloud App</h1>
+              <img src={portfolio1} className="items-center w-full" alt="" />
+              <h1 class="absolute text-xl font-medium text-white bottom-8 left-8">Cloud App</h1>
             </div> 
-            <div className='relative'>
-              <img src={portfolio2} alt="" />
+            <div className='relative p-5'>
+              <img src={portfolio2} className="items-center w-full" alt="" />
               <h1 class="absolute text-xl font-medium text-white bottom-8 left-8">Design Furniture App</h1>
             </div>   
           </div>
