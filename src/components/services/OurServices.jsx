43ecffcf96ebd457dc3cb3a08fa5af7ffaa196a9
Elton Mehmeti <97539294+EltonMehmeti@users.noreply.github.com>
2022-08-19 19:42:12 +0200

    changes

--- src/components/services/OurServices.jsx
+++ src/components/services/OurServices.jsx
@@ -10,16 +10,16 @@ const OurServices = () => {
 
     <div className="lg:flex lg:h-screen lg:bg-white " id='o'>
       <div className=" mt-10 lg:flex lg:flex-1 lg:items-center lg:justify-center" id='left'>
-        <div id='left-wrapper' className="text-center lg:flex lg:w-3/5 lg:h-4/5 lg:p-100px lg:flex-col lg:justify-center lg:gap-y-6 lg:text-left">
+        <div id='left-wrapper' className="text-center lg:flex lg:w-3/5 lg:h-4/5 lg:p-100px lg:flex-col lg:justify-center lg:gap-y-6">
           <h3 className="text-2xl mb-2 lg:text-left lg:text-3xl lg:font-light text-blue-600">Our Services</h3>
           <h1 className=" text-2xl  mb-6 lg:text-left lg:text-6xl">Perfect and fast Movement</h1>
-          <p className='lg:text-left md:px-10 mb-4 text-gray-500'>We move with make a Creative Strategy for help your business goal, we help to improve your income by a services we have. make your content look interesting and make people look for your business</p>
+          <p className='lg:text-left  mb-4 text-gray-500'>We move with make a Creative Strategy for help your business goal, we help to improve your income by a services we have. make your content look interesting and make people look for your business</p>
           <button type="button" className="mb-20 text-white bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 w-32">Read More...</button>
         </div>
       </div>
 
       <div className=" lg:flex-1 lg:relative" id='right'>
-        <div id='cards' className="flex flex-wrap items-center justify-center md:gap-x-6  lg:flex lg:flex-wrap lg:w-10/12 lg:h-4/5  lg:justify-between lg:gap-8 lg:mt-20 lg:overflow-hidden">
+        <div id='cards' className="flex flex-wrap items-center justify-center md:gap-x-6 xl:overflow-hidden  lg:flex lg:flex-wrap md:overflow-scroll lg:w-10/12 lg:h-4/5  lg:justify-between lg:gap-8 lg:mt-20 ">
           <a href="/#" className=" mb-4 block p-6 w-64 h-64  bg-white rounded-3xl border border-gray-200 shadow-md hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
             <img
             src={social}
