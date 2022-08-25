0ce529db72484d081372aa7e2ece724610a02af6
Albert <ialbertbislimi@gmail.com>
2022-08-25 11:49:35 +0200

    small changes

--- src/components/cta/Cta.jsx
+++ src/components/cta/Cta.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
-import collaboration from '../../assets/collaboration.png'
+import people from '../../assets/people.png'
 
 const Cta = () => {
   return (
 
-    <div className="lg:flex lg:h-screen lg:bg-white ">
+    <div className="lg:flex lg:bg-white ">
       <div className=" mt-10 lg:flex lg:flex-1 lg:items-center lg:justify-center">
         <div className="text-center lg:flex lg:w-3/5 lg:h-4/5 lg:p-100px lg:flex-col lg:justify-center lg:gap-y-6">
           <h1 className=" text-2xl mb-4 lg:text-left lg:text-6xl">Interesting Collaboration With Us?</h1>
@@ -16,7 +16,7 @@ const Cta = () => {
       <div className="m-10 lg:flex-1 lg:relative">
         <div className="flex flex-wrap items-center justify-center md:gap-x-6 xl:overflow-hidden lg:flex lg:flex-wrap md:overflow-scroll lg:justify-between lg:gap-8 lg:mt-20 ">
             <div className='relative'>
-                <img src={collaboration} className="items-center w-full" alt="" />
+                <img src={people} className="items-center w-full" alt="" />
             </div> 
         </div>
       </div>
