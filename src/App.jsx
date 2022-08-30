e1d481f32445e69c02e9e03437c9ad84276be5a0
Albert <ialbertbislimi@gmail.com>
2022-08-30 14:43:32 +0200

    small changes

--- src/App.jsx
+++ src/App.jsx
@@ -15,7 +15,7 @@ const App = () => {
       
   
       <div className='overflow-x-hidden dark:bg-black container mx-auto px-[25px] lg:px-[75px]'>
-      <div style={{ backgroundImage:`url(${bg})`,backgroundRepeat:"no-repeat",backgroundSize:"cover" }}>
+      <div style={{ backgroundImage:`url(${bg})`,backgroundRepeat:"no-repeat",backgroundSize:"cover"}}>
       <Nav />
       <Header />
         <About />
